2df69717f8cb3855862e21411cf5de053af291eb
IdaThomassen <eaa25ict@students.eaaa.dk>
2025-10-15 10:58:23 +0200

    opdatering
    opdatering af koden

---
+++ js/js.js
@@ -0,0 +1,12 @@
+console.log("Heloooo");
+
+document.querySelector("#fiskIda").addEventListener("click", function () {
+  window.location.href = "infopincetfisk.html";
+});
+
+/* brug til at lukke med, mangler en knap
+function lukPopup() {
+  const popup = document.querySelector("#fiskIda");
+  popup.style.display = "none";
+}
+  */
