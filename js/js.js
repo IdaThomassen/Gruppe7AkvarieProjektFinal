231a0acebe8b7f480dc70e333745d67af67fb933
IdaThomassen <eaa25ict@students.eaaa.dk>
2025-10-15 12:49:49 +0200

    næsten færdig
    næsten færdig

--- js/js.js
+++ js/js.js
@@ -8,6 +8,10 @@ function lukkeKnap(){
     window.location.href = "index.html";
 }
 
+document.querySelector("#puslespilKnap").addEventListener("click", function () {
+  window.location.href = "puslespil.html";
+});
+
 /*
 function lukkeKnap() {
   const knap = document.createElement("button");
